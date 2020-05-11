1bf13baf702264848bc0d1274c2d2a9ef43d729c
Adam Chalmers <achalmers@cloudflare.com>
2020-05-10 19:46:20 -0500

    Change actix-web integration to always return httpresponse

--- src/web.rs
+++ src/web.rs
@@ -7,24 +7,27 @@ use serde::Serialize;
 use serde_json;
 
 pub trait ResultExt {
-    fn json_response(self) -> serde_json::Result<HttpResponse>;
+    fn json_response(self) -> HttpResponse;
 }
 
 impl<T> ResultExt for Result<T, Error<StatusCode>>
 where
     T: Serialize,
 {
-    fn json_response(self) -> serde_json::Result<HttpResponse> {
-        match self {
-            Ok(t) => match serde_json::to_string(&t) {
-                Ok(j) => Ok(HttpResponse::Ok()
-                    .header(header::CONTENT_TYPE, "application/json")
-                    .body(j)),
-                Err(e) => Err(e),
-            },
-            Err(err) => Ok(HttpResponse::build(err.external.0)
+    fn json_response(self) -> HttpResponse {
+        match self.map(|s| serde_json::to_string(&s)) {
+            // Result OK
+            Ok(Ok(j)) => HttpResponse::Ok()
                 .header(header::CONTENT_TYPE, "application/json")
-                .body(to_json_err(err.external.1))),
+                .body(j),
+            // Result OK but couldn't deserialize into JSON
+            Ok(Err(e)) => HttpResponse::InternalServerError()
+                .header(header::CONTENT_TYPE, "application/json")
+                .body(format!("{}", e)),
+            // Result err
+            Err(err) => HttpResponse::build(err.external.0)
+                .header(header::CONTENT_TYPE, "application/json")
+                .body(to_json_err(err.external.1)),
         }
     }
 }
