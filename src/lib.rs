5fda079b9796db29b7245c843914bd90b9a8020b
Adam Chalmers <achalmers@cloudflare.com>
2020-05-10 20:03:27 -0500

    Use HttpError struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,24 @@
 #[cfg(feature = "actix_web")]
 mod web;
 
-use std::fmt;
+use std::fmt::{Display, Error as FmtError, Formatter};
 
-pub struct Error<Data> {
+pub struct Error<Data: Display> {
     pub internal: anyhow::Error,
-    pub user_data: Data,
-    pub user_text: &'static str,
+    pub external: Data,
 }
 
-impl<Data> Error<Data> {
-    pub fn describe<E: Into<anyhow::Error>>(
-        e: E,
-        user_data: Data,
-        user_text: &'static str,
-    ) -> Self {
+impl<Data: Display> Error<Data> {
+    pub fn describe<E: Into<anyhow::Error>>(e: E, external: Data) -> Self {
         Self {
             internal: e.into(),
-            user_data,
-            user_text,
+            external,
         }
     }
 }
 
-impl<Data> fmt::Display for Error<Data> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        write!(f, "{}", self.user_text)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let io_err = std::fs::read_to_string("not-a-real-file-path").unwrap_err();
-        let err = Error::describe(io_err, 404, "invalid file path");
-        println!("{}", err);
+impl<Data: Display> Display for Error<Data> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        write!(f, "{}", self.external)
     }
 }
--- src/web.rs
+++ src/web.rs
@@ -5,12 +5,24 @@ use actix_web::{
 };
 use serde::Serialize;
 use serde_json;
+use std::fmt::{Display, Error as FmtError, Formatter};
 
 pub trait ResultExt {
     fn json_response(self) -> HttpResponse;
 }
 
-impl<T> ResultExt for Result<T, Error<StatusCode>>
+pub struct HttpError {
+    pub code: StatusCode,
+    pub text: &'static str,
+}
+
+impl Display for HttpError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        self.text.fmt(f)
+    }
+}
+
+impl<T> ResultExt for Result<T, Error<HttpError>>
 where
     T: Serialize,
 {
@@ -20,19 +32,23 @@ where
             Ok(Ok(j)) => HttpResponse::Ok()
                 .header(header::CONTENT_TYPE, "application/json")
                 .body(j),
-            // Result OK but couldn't deserialize into JSON
-            Ok(Err(e)) => HttpResponse::InternalServerError()
-                .header(header::CONTENT_TYPE, "application/json")
-                .body(format!("{}", e)),
+            // Result OK but couldn't deserialize into JSON, so
+            // respond with the JSON error.
+            Ok(Err(e)) => {
+                eprintln!("{}", e);
+                HttpResponse::InternalServerError()
+                    .header(header::CONTENT_TYPE, "application/json")
+                    .body("Couldn't construct the JSON response")
+            }
             // Result err
-            Err(err) => HttpResponse::build(err.user_data)
+            Err(err) => HttpResponse::build(err.external.code)
                 .header(header::CONTENT_TYPE, "application/json")
-                .body(to_json_err(err.user_text)),
+                .body(to_json_err(&err.to_string())),
         }
     }
 }
 
-fn to_json_err(s: &'static str) -> String {
+fn to_json_err(s: &str) -> String {
     format!("{{\"error\": \"{}\"}}", s)
 }
 
